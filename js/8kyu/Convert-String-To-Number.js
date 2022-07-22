9c81bace825158b8922290a85890c1d98866e256
Luis-Betancourt93 <betancourtluis619@gmail.com>
2022-07-22 10:52:09 -0700

    Convert String to Number

---
+++ js/8kyu/Convert-String-To-Number.js
@@ -0,0 +1,32 @@
+// Description
+// We need a function that can transform a string into a number.
+
+
+// I need to make function that takes in a string and turns into a number.
+// 
+
+// Parameters: Taking in a String
+// It will always be a string, never empty, and no special characters. Always be on one passed in. 
+
+// Return: What comes out, the result.
+// Ok, console.log the results.
+
+// Examples: If this is the end result, then we should've solved the question.
+  // str = '-7' -> 7
+  // str = '5' -> 5
+  // str = '100' -> 100
+
+// Pseudo Code:
+
+const stringToNumber = function(str){
+  
+  console.log(Number(str))
+  
+}
+
+// Arrow Function
+// const stringToNumber = str => console.log(Number(str))
+
+
+
+stringToNumber('-7')
