a3c2b8141b7c7db64f5ccbc14340b7f27a4c5bfc
Luis-Betancourt93 <betancourtluis619@gmail.com>
2022-08-04 21:48:52 -0700

    Code Practice

--- js/preview.js
+++ js/preview.js
@@ -46,3 +46,10 @@
 // You don't need to worry about validation, since arr1 and arr2 must be arrays with 0 or more Integers. If both arr1 and arr2 are empty, then just return an empty array.
 
 // Note: arr1 and arr2 may be sorted in different orders. Also arr1 and arr2 may have same integers. Remove duplicated in the returned result.
+
+
+// Write a function that will check if two given characters are the same case.
+
+// If either of the characters is not a letter, return -1
+// If both characters are the same case, return 1
+// If both characters are letters, but not the same case, return 0
