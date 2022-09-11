4734075f9547526b61ac13b3fac204b8e5cf6198
Luis-Betancourt93 <betancourtluis619@gmail.com>
2022-09-10 22:12:26 -0700

    Practice

--- js/preview.js
+++ js/preview.js
@@ -78,3 +78,17 @@ console.log (reverseString("hello world!"));
       return str.split(' ').reverse().join(' ');
     }
 */
+
+
+/*
+Messi goals function
+Messi is a soccer player with goals in three leagues:
+
+LaLiga
+Copa del Rey
+Champions
+Complete the function to return his total number of goals in all three leagues.
+
+Note: the input will always be valid.
+
+*/
