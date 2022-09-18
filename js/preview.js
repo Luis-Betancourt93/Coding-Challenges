929230a5aad2468e0db2ccf9144564b27539a47b
Luis-Betancourt93 <betancourtluis619@gmail.com>
2022-09-17 22:50:38 -0700

    Function Practice
    Returning a String

--- js/preview.js
+++ js/preview.js
@@ -171,3 +171,19 @@ While making a zork-type game, you create an object of rooms. Unfortunately, the
 
 // ----------- Solution ---------
 // Was missing some commas and perenthesis
+
+
+
+/*
+Returning a String Challange 
+
+Make a function that will return a greeting statement that uses an input; your program should return, "Hello, <name> how are you doing today?".
+[Make sure you type the exact thing I wrote or the program may not execute properly]
+*/
+
+/*
+----------- Solution ---------
+function greet(name){
+    return `Hello, ${name} how are you doing today?`
+  }
+  */
