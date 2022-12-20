d7a6c1ab9a3c3a7b2406bf1ac32f87b3b2612039
Luis-Betancourt93 <betancourtluis619@gmail.com>
2022-12-19 19:32:59 -0800

    Checking if all objects in the array have blue eyes

---
+++ js/Js_Methods.js/Does-Character-Have-Blue-Eyes.js
@@ -0,0 +1,38 @@
+// Given this array of objects check if every character have blue eye.
+
+
+const characters = [
+  {
+      name: 'Luke Skywalker',
+      height: '172',
+      mass: '77',
+      eye_color: 'blue',
+      gender: 'male',
+  },
+  {
+      name: 'Darth Vader',
+      height: '202',
+      mass: '136',
+      eye_color: 'yellow',
+      gender: 'male',
+  },
+  {
+      name: 'Leia Organa',
+      height: '150',
+      mass: '49',
+      eye_color: 'brown',
+      gender: 'female',
+  },
+  {
+      name: 'Anakin Skywalker',
+      height: '188',
+      mass: '84',
+      eye_color: 'blue',
+      gender: 'male',
+  },
+];
+
+
+const haveBlueEyes = characters.every(element => element.eye_color === 'blue')
+console.log(haveBlueEyes)
+// Should return 'False' because using .every() it take a 'Test/Condition' that all elements in the array should pass.
