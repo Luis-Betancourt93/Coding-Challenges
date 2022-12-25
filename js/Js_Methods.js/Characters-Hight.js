0456e30bd5a13d5cc67ed7057b5bcc0fdb5b4e9e
Luis-Betancourt93 <betancourtluis619@gmail.com>
2022-12-24 20:16:09 -0800

    Return new array with everyones height

---
+++ js/Js_Methods.js/Characters-Hight.js
@@ -0,0 +1,39 @@
+
+
+// Give me all the height of all the characters in an array
+
+
+const characters = [
+  {
+      name: 'Luke Skywalker',
+      height: '172',
+      mass: '77',
+      eye_color: 'blue',
+      gender: 'male',
+  },
+  {
+      name: 'Darth Vader',
+      height: '202',
+      mass: '136',
+      eye_color: 'yellow',
+      gender: 'male',
+  },
+  {
+      name: 'Leia Organa',
+      height: '150',
+      mass: '49',
+      eye_color: 'brown',
+      gender: 'female',
+  },
+  {
+      name: 'Anakin Skywalker',
+      height: '188',
+      mass: '84',
+      eye_color: 'blue',
+      gender: 'male',
+  },
+];
+
+
+const everyOnesHeight = characters.map(element => element.height)
+console.log(everyOnesHeight)
