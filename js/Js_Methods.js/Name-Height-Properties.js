a6ae1de1f02484031b4d3bda44e7b558ef5d3f92
Luis-Betancourt93 <betancourtluis619@gmail.com>
2022-12-29 20:24:17 -0800

    Return Name and Height

---
+++ js/Js_Methods.js/Name-Height-Properties.js
@@ -0,0 +1,37 @@
+
+// Get an array of objects with just names and height properties
+
+
+const characters = [
+  {
+      name: 'Luke Skywalker',
+      height: '172',
+      mass: '77',
+      eye_color: 'blue',
+      gender: 'male',
+  },
+  {
+      name: 'Darth Vader',
+      height: '202',
+      mass: '136',
+      eye_color: 'yellow',
+      gender: 'male',
+  },
+  {
+      name: 'Leia Organa',
+      height: '150',
+      mass: '49',
+      eye_color: 'brown',
+      gender: 'female',
+  },
+  {
+      name: 'Anakin Skywalker',
+      height: '188',
+      mass: '84',
+      eye_color: 'blue',
+      gender: 'male',
+  },
+];
+
+const nameAndHeight = characters.map(element => element.name + ' ' + element.height)
+console.log(nameAndHeight)
