80fbdbf9425112c662d46355a2ee87bac8dcb902
Luis-Betancourt93 <betancourtluis619@gmail.com>
2023-01-08 12:47:21 -0800

    Does every number pass the test

---
+++ js/Js_Methods.js/Is-Every-Number-Is-Grater-Than-Zero.js
@@ -0,0 +1,7 @@
+// Check if every number in this array is grater than zero. 
+
+let numbers = [1, 3, 5, 10, 43, 64, 8];
+
+console.log(numbers.every(element => element > 0))
+
+// Here we are using the .every() Method which takes in a 'test' condition and will test every element to see if it pass it. It will return a True or False.
