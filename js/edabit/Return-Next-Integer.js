f04cd438be238771d9892f49e8c770b86a0ab8e9
Luis-Betancourt93 <betancourtluis619@gmail.com>
2023-01-19 20:47:48 -0800

    Return next integer

---
+++ js/edabit/Return-Next-Integer.js
@@ -0,0 +1,14 @@
+// Create a function that takes a number as an argument, increments the number by +1 and returns the result.
+
+// Examples
+// addition(0) ➞ 1
+
+// addition(9) ➞ 10
+
+// addition(-3) ➞ -2
+
+// function addition(num) {
+// 	return num + 1
+// }
+
+const addition = num => num + 1
