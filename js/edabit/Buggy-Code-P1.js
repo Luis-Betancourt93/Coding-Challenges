a4af8252820e9b6eb6b9071a851586436fdaa2c3
Luis-Betancourt93 <betancourtluis619@gmail.com>
2023-01-22 16:08:37 -0800

    Buggy Code P1

---
+++ js/edabit/Buggy-Code-P1.js
@@ -0,0 +1,14 @@
+// Fix the code in the code tab to pass this challenge (only syntax errors). Look at the examples below to get an idea of what the function should do.
+
+// Examples
+// cubes(3) ➞ 27
+
+// cubes(5) ➞ 125
+
+// cubes(10) ➞ 1000
+
+function cubes(a) {
+	console.log( a ** 3 )
+}
+
+cubes(3)
