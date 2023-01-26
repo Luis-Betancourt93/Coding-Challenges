7b80bd8d89a9908a8ba6c737891ea8226fc2eba6
Luis-Betancourt93 <betancourtluis619@gmail.com>
2023-01-25 16:24:32 -0800

    Power Calculator

---
+++ js/edabit/Power-Calculator.js
@@ -0,0 +1,21 @@
+// Create a function that takes voltage and current and returns the calculated power.
+
+
+function circuitPower(voltage, current) {
+  console.log(voltage * current)
+  
+}
+
+
+
+
+
+
+
+
+// Examples
+circuitPower(230, 10) // ➞ 2300
+
+circuitPower(110, 3) // ➞ 330
+
+circuitPower(480, 20) // ➞ 9600
