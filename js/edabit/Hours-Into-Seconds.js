57550a35c0ea6aedb240217eadf75e81cc95e918
Luis-Betancourt93 <betancourtluis619@gmail.com>
2023-01-26 16:30:10 -0800

    Hours into seconds

---
+++ js/edabit/Hours-Into-Seconds.js
@@ -0,0 +1,24 @@
+// Notes
+// 60 seconds in a minute, 60 minutes in an hour
+// Don't forget to return your answer.
+
+// Write a function that converts hours into seconds.
+
+function howManySeconds(hours) {
+	console.log(hours * 60 * 60)
+
+
+}
+
+
+
+
+
+
+
+howManySeconds(2) // ➞ 7200
+
+howManySeconds(10) // ➞ 36000
+
+howManySeconds(24) // ➞ 86400
+
