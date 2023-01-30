d927dcc36f78288a8081ef0ee70fc1c3b677d991
Luis-Betancourt93 <betancourtluis619@gmail.com>
2023-01-29 16:41:56 -0800

    Return the remainder of 2 numbers

---
+++ js/edabit/Return-The-Remainder.js
@@ -0,0 +1,23 @@
+// Notes
+// The tests only use positive and negative integers.
+// Don't forget to return the result.
+// If you get stuck on a challenge, find help in the Resources tab.
+// If you're really stuck, unlock solutions in the Solutions tab.
+
+// There is a single operator in JavaScript, capable of providing the remainder of a division operation. Two numbers are passed as parameters. The first parameter divided by the second parameter will have a remainder, possibly zero. Return that value.
+
+function remainder(x, y) {
+console.log(x % y)	
+}
+
+
+
+
+
+remainder(1, 3) // ➞ 1
+
+remainder(3, 4) // ➞ 3
+
+remainder(-9, 45) // ➞ -9
+
+remainder(5, 5) // ➞ 0
