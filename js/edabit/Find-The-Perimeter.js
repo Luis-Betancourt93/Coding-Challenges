068457acd9b5b86de5be4f80c337e5b9dc41a6e9
Luis-Betancourt93 <betancourtluis619@gmail.com>
2023-01-30 16:54:10 -0800

    Find the perimeter of a triangle

---
+++ js/edabit/Find-The-Perimeter.js
@@ -0,0 +1,24 @@
+// Create a function that takes length and width and finds the perimeter of a rectangle.
+
+
+
+
+function findPerimeter(length, width) {
+
+console.log(length * 2 + width * 2)
+}
+
+
+
+
+
+
+
+
+
+
+findPerimeter(6, 7) // ➞ 26
+
+findPerimeter(20, 10) // ➞ 60
+
+findPerimeter(2, 9) // ➞ 22
