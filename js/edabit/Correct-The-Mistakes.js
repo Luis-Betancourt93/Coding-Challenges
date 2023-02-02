93a5ff2b1bb5d2374ce255f2376867ba7973b761
Luis-Betancourt93 <betancourtluis619@gmail.com>
2023-02-01 17:05:07 -0800

    Correct the mistakes

---
+++ js/edabit/Correct-The-Mistakes.js
@@ -0,0 +1,15 @@
+// Fix the code in the code tab to pass this challenge (only syntax errors). Look at the examples below to get an idea of what the function should do.
+
+
+function squared(b) {
+	console.log( b * b)
+}
+
+
+
+
+squared(5) // ➞ 25
+
+squared(9) // ➞ 81
+
+squared(100) // ➞ 10000
