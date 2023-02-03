b920e26107df9b6d352ae7d0f50732368f0eaea0
Luis-Betancourt93 <betancourtluis619@gmail.com>
2023-02-02 17:13:10 -0800

    Is the number less than zero

---
+++ js/edabit/Is-The-Number-Less-Than-Zero.js
@@ -0,0 +1,20 @@
+// Create a function that takes a number as its only argument and returns true if it's less than or equal to zero, otherwise return false.
+
+
+
+
+function lessThanOrEqualToZero(num) {
+	if (num <= 0) {
+    console.log(true)
+  } else {
+    console.log(false)
+  }
+}
+
+
+
+lessThanOrEqualToZero(5) // ➞ false
+
+lessThanOrEqualToZero(0) // ➞ true
+
+lessThanOrEqualToZero(-2) // ➞ true
