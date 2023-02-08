dcdad8a72895da1573413c8f58d4c8b1449c5060
Luis-Betancourt93 <betancourtluis619@gmail.com>
2023-02-07 17:45:59 -0800

    Basic Variable Assignment

---
+++ js/edabit/Basic-Variable-Assignment.js
@@ -0,0 +1,16 @@
+// A student learning JavaScript was trying to make a function. His code should concatenate a passed string name with string "Edabit" and store it in a variable called result. He needs your help to fix this code.
+
+
+
+function nameString(name){
+	var b = "Edabit"
+	var result = name + b
+  	console.log(result) 
+}
+
+
+nameString("Mubashir") // ➞ "MubashirEdabit"
+
+nameString("Matt") // ➞ "MattEdabit"
+
+nameString("javaScript") // ➞ "javaScriptEdabit"
