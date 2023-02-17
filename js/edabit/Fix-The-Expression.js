5c536eee11e83a7ad1c6c7429138a9ac88a1fdd4
Luis-Betancourt93 <betancourtluis619@gmail.com>
2023-02-16 19:02:20 -0800

    Fix the expression

---
+++ js/edabit/Fix-The-Expression.js
@@ -0,0 +1,13 @@
+// Fix the code in the Code tab so the function returns true if and only if x is equal to 7.
+
+function isSeven(x) {
+console.log( x === 7 ? true : false)
+}
+
+
+
+isSeven(4) // ➞ false
+
+isSeven(9) // ➞ false
+
+isSeven(7) // ➞ true
