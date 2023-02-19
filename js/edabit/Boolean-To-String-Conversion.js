4754f633b7e6436f9e870e1a997a2402a0bc3114
Luis-Betancourt93 <betancourtluis619@gmail.com>
2023-02-19 15:39:30 -0800

    Boolean to string

---
+++ js/edabit/Boolean-To-String-Conversion.js
@@ -0,0 +1,11 @@
+// Create a function that takes a boolean variable flag and returns it as a string.
+
+function boolToString(flag) {
+  const booToString = flag.toString()
+	console.log(String(booToString))
+}
+
+
+boolToString(true) // ➞ "true"
+
+boolToString(false) // ➞ "false"
