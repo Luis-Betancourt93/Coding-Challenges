2b8b0a2c330f3c31c49a44259e08585f1537d96e
Luis-Betancourt93 <betancourtluis619@gmail.com>
2023-02-20 15:43:07 -0800

    Using Arrow Functions

---
+++ js/edabit/Using-Arrow-Functions.js
@@ -0,0 +1,12 @@
+// Create a function that returns the given argument, but by using an arrow function.
+
+// An arrow function is constructed like so: arrowFunc=(/*parameters*/)=>//code here
+
+
+const arrowFunc = param => console.log(param)
+
+arrowFunc(3) // ➞ 3
+
+arrowFunc("3") // ➞ "3"
+
+arrowFunc(true) // ➞ true
