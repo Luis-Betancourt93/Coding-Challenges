1c5b542ae81c4ebcb1eca39996fb3df575ee7f8c
Luis-Betancourt93 <betancourtluis619@gmail.com>
2023-02-25 17:15:19 -0800

    Pair management

---
+++ js/edabit/Pair-management.js
@@ -0,0 +1,19 @@
+// Given two arguments, return an array which contains these two arguments.
+
+function makePair(num1, num2) {
+  const newArray = makePair.split()
+
+	console.log(newArray)
+}
+
+
+
+
+
+
+
+makePair(1, 2) // ➞ [1, 2]
+
+makePair(51, 21) // ➞ [51, 21]
+
+makePair(512124, 215) // ➞ [512124, 215]
