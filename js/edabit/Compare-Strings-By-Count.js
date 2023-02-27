a8249dd5bf2561e19a921ffbf163c03092c14b4d
Luis-Betancourt93 <betancourtluis619@gmail.com>
2023-02-26 17:18:37 -0800

    Compare Strings by count

---
+++ js/edabit/Compare-Strings-By-Count.js
@@ -0,0 +1,18 @@
+// Create a function that takes two strings as arguments and return either true or false depending on whether the total number of characters in the first string is equal to the total number of characters in the second string.
+
+function comp(str1, str2) {
+	if (str1.length === str2.length) {
+    console.log(true)
+  } else {
+    console.log(false)
+  }
+}
+
+
+
+
+comp("AB", "CD") // ➞ true
+
+comp("ABC", "DE") // ➞ false
+
+comp("hello", "edabit") // ➞ false
