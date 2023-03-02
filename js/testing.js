20203ff93eeecb4d2a652d7b7c84d118f2298e77
Luis-Betancourt93 <betancourtluis619@gmail.com>
2023-03-02 13:08:41 -0800

    Sum Mixed Arrays

--- js/testing.js
+++ js/testing.js
@@ -1,11 +1,4 @@
 
-const numbers = []
 
-// const sum = numbers.reduce((total, number) => {
-//  return total + number
-// }, 0)
 
-const sum = numbers.reduce((acc, num) => acc + num, 0)
-
-console.log(sum)
 
