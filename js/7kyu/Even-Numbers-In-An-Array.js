30b05a3259b2d7bb191092bcc073c2529d5f8387
Luis-Betancourt93 <betancourtluis619@gmail.com>
2023-03-04 22:07:44 -0800

    String Ends with

--- js/7kyu/Even-Numbers-In-An-Array.js
+++ js/7kyu/Even-Numbers-In-An-Array.js
@@ -14,18 +14,15 @@
 
 // SOLUTION
 
-// function evenNumbers(array, number) {
-//   const evenNums = array.filter(element => element % 2 === 0).slice(-number)
-//   console.log(evenNums)
-// }
+function evenNumbers(array, number) {
+  const evenNums = array.filter(element => element % 2 === 0).slice(-number)
+  console.log(evenNums)
+}
 
 
 // ARROW FUNCTION
 
 const evenNumber = (array, number) => array.filter(element => element % 2 === 0).slice(-number)
-
-
-
 
 evenNumbers([1, 2, 3, 4, 5, 6, 7, 8, 9],3)
 evenNumbers([-22, 5, 3, 11, 26, -6, -7, -8, -9, -8, 26], 2)
---
+++ js/7kyu/String-Ends-With.js
@@ -0,0 +1,20 @@
+// Complete the solution so that it returns true if the first argument(string) passed in ends with the 2nd argument (also a string).
+
+// Examples:
+
+
+
+
+
+
+function solution(str, ending){
+  
+  if (str.endsWith(ending)  ) {
+    return true
+  } else {
+    return false
+  }
+}
+
+solution('abc', 'bc') // returns true
+solution('abc', 'd') // returns false
