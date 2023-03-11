f269d82384c9fd07141c698ebf54612ac9cfa34a
Luis-Betancourt93 <betancourtluis619@gmail.com>
2023-03-10 17:37:13 -0800

    Is string empty

---
+++ js/edabit/Is-The-String-Empty.js
@@ -0,0 +1,22 @@
+// Create a function that returns true if a string is empty and false otherwise.
+
+
+function isEmpty(s) {
+	
+if (s.length === 0) {
+  console.log(true)
+} else {
+  console.log(false)
+}
+
+
+}
+
+
+
+
+isEmpty("") // ➞ true
+
+isEmpty(" ") // ➞ false
+
+isEmpty("a") // ➞ false
