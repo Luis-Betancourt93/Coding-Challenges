9bbaf74c01302d7a5cde73799d49c252cfc253bb
Luis-Betancourt93 <betancourtluis619@gmail.com>
2023-03-11 17:55:40 -0800

    Check if devisible by five

---
+++ js/edabit/Check-If-Devisible-By-5.js
@@ -0,0 +1,22 @@
+// Create a function that returns true if an integer is evenly divisible by 5, and false otherwise.
+
+function divisibleByFive(n) {
+	
+if (n % 5 === 0) {
+   console.log(true)
+} else {
+  console.log(false)
+}
+
+
+}
+
+
+
+
+
+divisibleByFive(5) // ➞ true
+
+divisibleByFive(-55) // ➞ true
+
+divisibleByFive(37) // ➞ false
