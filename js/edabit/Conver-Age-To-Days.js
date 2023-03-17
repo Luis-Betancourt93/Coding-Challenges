1450aa050ec5aee0f32260b8ec717d66d0e89432
Luis-Betancourt93 <betancourtluis619@gmail.com>
2023-03-16 17:04:20 -0700

    Convert age to days

---
+++ js/edabit/Conver-Age-To-Days.js
@@ -0,0 +1,17 @@
+// Create a function that takes the age in years and returns the age in days.
+
+// Examples
+// calcAge(65) ➞ 23725
+
+// calcAge(0) ➞ 0
+
+// calcAge(20) ➞ 7300
+
+
+// function calcAge(age) {
+// return age * 365
+  
+// }
+
+
+const calcAge = age => age * 365
