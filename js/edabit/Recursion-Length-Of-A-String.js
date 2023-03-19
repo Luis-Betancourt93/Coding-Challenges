dc8ee3f40edc0e3711424fbda1512ad2bd3aaa4d
Luis-Betancourt93 <betancourtluis619@gmail.com>
2023-03-18 19:11:19 -0700

    Recursion Lenth of a string

---
+++ js/edabit/Recursion-Length-Of-A-String.js
@@ -0,0 +1,14 @@
+// Write a function that returns the length of a string. Make your function recursive.
+
+function length(str) {
+	console.log(str.length)
+}
+
+
+length("apple") // ➞ 5
+
+length("make") // ➞ 4
+
+length("a") // ➞ 1
+
+length("") // ➞ 0
