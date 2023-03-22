260d2c7957d75c6d3e947f9992098b355df6a80b
Luis-Betancourt93 <betancourtluis619@gmail.com>
2023-03-21 19:21:07 -0700

    Return string as an interger

---
+++ js/edabit/Return-String-As-An-Interger.js
@@ -0,0 +1,14 @@
+// Create a function that takes a string and returns it as an integer.
+
+function stringInt(str) {
+	console.log(Number(str))
+}
+
+
+
+
+stringInt("6") // ➞ 6
+
+stringInt("1000") // ➞ 1000
+
+stringInt("12") // ➞ 12
