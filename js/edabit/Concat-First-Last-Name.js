9f7a4492864add240d1ad79538d4b4f752ac440a
Luis-Betancourt93 <betancourtluis619@gmail.com>
2023-03-23 19:47:30 -0700

    Concat first and last names

---
+++ js/edabit/Concat-First-Last-Name.js
@@ -0,0 +1,17 @@
+// Given two strings, firstName and lastName, return a single string in the format "last, first".
+
+
+function concatName(firstName, lastName) {
+	
+console.log(`${lastName}, ${firstName}`)
+
+
+}
+
+
+
+concatName("First", "Last") // ➞ "Last, First"
+
+concatName("John", "Doe") // ➞ "Doe, John"
+
+concatName("Mary", "Jane") // ➞ "Jane, Mary"
