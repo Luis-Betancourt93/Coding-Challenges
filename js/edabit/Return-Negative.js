0c05c9bcafcf3f632b90dcb3ca704487f859f643
Luis-Betancourt93 <betancourtluis619@gmail.com>
2023-03-25 20:01:14 -0700

    Return Negative

---
+++ js/edabit/Return-Negative.js
@@ -0,0 +1,15 @@
+// Create a function that takes a number as an argument and returns negative of that number. Return negative numbers without any change.
+
+
+function returnNegative(n) {
+	console.log(Math.abs(n))
+}
+
+
+returnNegative(4) // ➞ -4
+
+returnNegative(15) // ➞ -15
+
+returnNegative(-4) // ➞ -4
+
+returnNegative(0) // ➞ 0
