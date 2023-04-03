b1d9bedeccd0985fc58321efe8ebed093164aedb
Luis-Betancourt93 <betancourtluis619@gmail.com>
2023-04-02 19:58:02 -0700

    Arrow Functions

---
+++ js/edabit/Arrow-Functions.js
@@ -0,0 +1,41 @@
+// In the Code tab you will find code that is missing a single character in order to pass the tests. However, your goal is to submit a function as minimalist as possible. Use the tips in the tips section below.
+
+// Write five adder functions:
+
+
+
+// function add2(x) {
+// 	return x + 2;
+// }
+
+const add2 = x => console.log(x + 2)
+
+// function add3(x) {
+// 	return x + 3;
+// }
+
+const add3 = x => console.log(x + 3)
+
+// function add5(x) {
+// 	return x + 5;
+// }
+
+const add5 = x => console.log(x + 5)
+
+// function add7(x) {
+// 	return x + 7;
+// }
+
+const add7 = x => console.log(x + 7)
+
+// function add11(x) {
+// 	return x + 1;
+// }
+
+const add11 = x => console.log(x + 1)
+
+add2(5) // should return 2 + x.
+add3(5) // should return 3 + x.
+add5(5) // should return 5 + x.
+add7(5) // should return 7 + x.
+add11(5) // should return 11 + x.
