b3c73366f141ddf13e314a072d038799ab619229
Luis-Betancourt93 <betancourtluis619@gmail.com>
2023-04-08 22:17:19 -0700

    Solve The Equation

---
+++ js/edabit/Solve-The-Equation.js
@@ -0,0 +1,19 @@
+// Create a function that takes an equation (e.g. "1+1"), and returns the answer.
+
+
+function equation(s) {
+	console.log(eval(s))
+}
+
+
+
+
+
+
+
+
+equation("1+1")  //➞ 2
+
+equation("7*4-2")  //➞ 26
+
+equation("1+1+1+1+1") // ➞ 5
