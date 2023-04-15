7dd188d2a63316b716539ac2d3c0ef23562c2125
Luis-Betancourt93 <betancourtluis619@gmail.com>
2023-04-14 22:09:47 -0700

    To The Power

---
+++ js/edabit/To-The-Power.js
@@ -0,0 +1,12 @@
+// Create a function that takes a base number and an exponent number and returns the calculation.
+
+function calculateExponent(num, exp) {
+	console.log(num ** exp)
+}
+
+
+calculateExponent(5, 5) // ➞ 3125
+
+calculateExponent(10, 10) // ➞ 10000000000
+
+calculateExponent(3, 3) // ➞ 27
