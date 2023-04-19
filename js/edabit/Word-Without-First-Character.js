da33e5ac722952e0f01e1ae417d944aa90bada7a
Luis-Betancourt93 <betancourtluis619@gmail.com>
2023-04-18 22:22:53 -0700

    Word without first character

---
+++ js/edabit/Word-Without-First-Character.js
@@ -0,0 +1,13 @@
+// Create a function that takes a word and returns the new word without including the first character.
+
+function newWord(str) {
+	console.log(str.substring(1))
+}
+
+
+
+newWord("apple") // ➞ "pple"
+
+newWord("cherry") // ➞ "herry"
+
+newWord("plum") // ➞ "lum"
