00cf9065fcd84c545467968caad29ea63a4ce5cb
Luis-Betancourt93 <betancourtluis619@gmail.com>
2023-04-22 14:06:19 -0700

    Name Greeting

---
+++ js/edabit/Name-Greeting.js
@@ -0,0 +1,10 @@
+// Create a function that takes a name and returns a greeting in the form of a string. Don't use a normal function, use an arrow function.
+
+const helloName = name => console.log(`"Hello ${name}!"`)
+
+
+helloName("Gerald") // ➞ "Hello Gerald!"
+
+helloName("Tiffany") // ➞ "Hello Tiffany!"
+
+helloName("Ed") // ➞ "Hello Ed!"
