1bfe656ab68403db06ba200dec4ccfdae68e2773
Luis-Betancourt93 <betancourtluis619@gmail.com>
2023-04-29 14:27:05 -0700

    Find Index Pt1

---
+++ js/edabit/Find-Index-P1.js
@@ -0,0 +1,14 @@
+// Create a function that finds the index of a given item.
+
+function search(arr, item) {
+	console.log(arr.indexOf(item))
+}
+
+
+
+// Examples
+search([1, 5, 3], 5) // ➞ 1
+
+search([9, 8, 3], 3) // ➞ 2
+
+search([1, 2, 3], 4) // ➞ -1
