6667905a47e18fc1810b0e6740efef2dba8d30e2
Luis-Betancourt93 <betancourtluis619@gmail.com>
2023-05-27 22:37:08 -0700

    Recreating the abs() function

---
+++ js/edabit/Recreating-The-abs-Function.js
@@ -0,0 +1,22 @@
+// The Math.abs() function returns the absolute value of a number. This means that it returns a number's positive value. You can think of it as the distance away from zero.
+
+// Create a function that recreates this functionality.
+
+function absolute(n) {
+	console.log(n < 0 ? n * -1 : n )
+}
+
+
+
+
+absolute(-1.217197940)  //➞ 1.21719794
+
+absolute(-12.1320)  //➞ 12.132
+ 
+absolute(-544.0)  //➞ 544
+
+absolute(4666)  //➞ 4666
+
+absolute(-3.14)  //➞ 3.14
+
+absolute(250) // ➞ 250
