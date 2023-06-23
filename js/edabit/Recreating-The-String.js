fa13c7f9d73ba50226a7f93a0988d48928fe8418
Luis-Betancourt93 <betancourtluis619@gmail.com>
2023-06-23 10:19:27 -0700

    Recreating the String

---
+++ js/edabit/Recreating-The-String.js
@@ -0,0 +1,13 @@
+/*
+Create a function which returns the length of a string,
+*/
+
+length("Hello World") // ➞ 11
+
+length("Edabit") // ➞ 6
+
+length("wash your hands!") //➞ 16
+
+function length(str) {
+  console.log(str.length)
+}
