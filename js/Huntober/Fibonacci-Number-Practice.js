58fe3dab7466b597a7025dad2dfd1cee67f452d5
Luis-Betancourt93 <betancourtluis619@gmail.com>
2023-06-29 17:04:13 -0700

    Practice some Fibonacci number problem

---
+++ js/Huntober/Fibonacci-Number-Practice.js
@@ -0,0 +1,15 @@
+/*
+Practicing Fibonacci Problem, Huntober Jan 4 2023
+*/
+
+function fib(num) {
+  if( num < 2 ){
+    return num
+  }
+
+  return fib(num - 1) + fib(num - 2)
+
+}
+
+console.log(fib(5))
+console.log(fib(4))
