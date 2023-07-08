acfcf927007049c8fe2a9460a1cc72d4454ad5bb
Luis-Betancourt93 <betancourtluis619@gmail.com>
2023-07-08 16:15:12 -0700

    Use the method to add every Num in the array

---
+++ js/edabit/Add-Every-Number.js
@@ -0,0 +1,9 @@
+/*
+Make a new variable called 'Sum' and add every number in this array to it.
+Use the arrow function to solve this problem 
+*/
+
+const number = [1, 3, 2, 8] // 14
+
+// Delete all code under here when done
+
