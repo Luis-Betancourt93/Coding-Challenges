60f58f57bbec9e54f2a57038223308393db6ae50
Luis-Betancourt93 <betancourtluis619@gmail.com>
2023-07-16 16:45:26 -0700

    Buurrrrp

---
+++ js/edabit/Burrrrp.js
@@ -0,0 +1,14 @@
+/*
+  Create a function that returns the string "Burp" with the amount of "r's" determined by the input parameters of the function.
+*/
+
+longBurp(3) // ➞ "Burrrp"
+
+longBurp(5) // ➞ "Burrrrrp"
+
+longBurp(9) // ➞ "Burrrrrrrrrp"
+
+function longBurp(num) {
+
+	console.log(`burrr`)
+}
