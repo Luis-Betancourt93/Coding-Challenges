195595494ae8f131de21571cf64cac02d5609714
Luis-Betancourt93 <betancourtluis619@gmail.com>
2023-07-31 18:19:00 -0700

    Front 3 letters

---
+++ js/edabit/Front-3.js
@@ -0,0 +1,14 @@
+/*
+Create a function that takes a string; we'll say that the front is the first three characters of the string. If the string length is less than three characters, the front is whatever is there. Return a new string, which is three copies of the front.
+*/
+
+frontThree("Python") // ➞ "PytPytPyt"
+
+frontThree("Cucumber") // ➞ "CucCucCuc"
+
+frontThree("bioshock") // ➞ "biobiobio"
+
+function frontThree(word) {
+  let front3 = word.slice(0, 3)
+  console.log(front3.repeat(3))
+}
