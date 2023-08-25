42603dfd988f0ce75a04839845ca8bd3bd5267b9
Luis-Betancourt93 <betancourtluis619@gmail.com>
2023-08-24 19:44:17 -0700

    Recreating string length

---
+++ js/edabit/Recreating-String.js
@@ -0,0 +1,13 @@
+// Return the length of the string
+
+function length(str) {
+  console.log(str.length)
+}
+
+
+
+length("Hello World") // ➞ 11
+
+length("Edabit") // ➞ 6
+
+length("wash your hands!") // ➞ 16
