9c12afbfe70b0c174cd0191cf21248b99db188df
Luis-Betancourt93 <betancourtluis619@gmail.com>
2023-08-29 10:49:17 -0700

    Reapeat String

---
+++ js/edabit/Reapeat-String.js
@@ -0,0 +1,19 @@
+/*
+  Create a function that takes a string txt and a number n and returns the repeated string n number of times.
+
+*/
+
+function repeatString(str, num) {
+
+  console.log(str.repeat(num))
+}
+
+
+
+
+
+
+repeatString("Mubashir", 2) // ➞ "MubashirMubashir"
+
+repeatString("Matt", 3) // ➞ "MattMattMatt"
+
