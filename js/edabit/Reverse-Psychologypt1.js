94fe11b87eccde251cebc1da22633645949750a2
Luis-Betancourt93 <betancourtluis619@gmail.com>
2023-08-31 14:07:13 -0700

    Reverse psychology pt1

---
+++ js/edabit/Reverse-Psychologypt1.js
@@ -0,0 +1,21 @@
+/*
+For this challenge, you will NOT be given a string. Your task isn't to add "Do not" before the given string. If there is no given string, you will not return "Do not do anything." Do not check the examples to know how to do this challenge.
+
+*/
+
+
+function reversePsychology(str) {
+  console.log(`Do not ${str}`)
+
+}
+
+
+
+
+
+
+reversePsychology("wash the dishes") // ➞ "Do not wash the dishes."
+
+reversePsychology("eat your lunch") // ➞ "Do not eat your lunch."
+
+reversePsychology("go to school") // ➞ "Do not go to school."
