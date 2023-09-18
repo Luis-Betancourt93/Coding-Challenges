feb8b70b922831ca787000e6dff4d42dd6a3faf5
Luis-Betancourt93 <betancourtluis619@gmail.com>
2023-09-18 16:28:40 -0700

    Who likes it?

--- JS-Challenges/11-Get-The-Middle-Chr.js
+++ JS-Challenges/11-Get-The-Middle-Chr.js
@@ -13,8 +13,3 @@ console.log(getMiddle('test')); // 'es'
 console.log(getMiddle('testing')); // 't'
 console.log(getMiddle('middle')); // 'dd'
 console.log(getMiddle('A')); // 'A'
-
-console.log(getMiddle('test')); // 'es'
-console.log(getMiddle('testing')); // 't'
-console.log(getMiddle('middle')); // 'dd'
-console.log(getMiddle('A')); // 'A'
