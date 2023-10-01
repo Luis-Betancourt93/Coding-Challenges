bbd9d80a4555709bf569e0ffba823661a6fae9e1
Luis-Betancourt93 <betancourtluis619@gmail.com>
2023-09-30 21:35:13 -0700

    Square every digit

---
+++ JS-Challenges/14-Square-Every-Digit.js
@@ -0,0 +1,18 @@
+/*
+Given an integer, your task is to square every digit of it and concatenate them to produce a new integer.
+*/
+
+
+const squareDigits = num => {
+  return Number(
+    num
+      .toString()
+      .split('')
+      .map(ele => ele * ele)
+      .join('')
+  );
+};
+
+console.log(squareDigits(2112)); // 4114
+console.log(squareDigits(3212)); // 9414
+console.log(squareDigits(9159)); // 8112581
