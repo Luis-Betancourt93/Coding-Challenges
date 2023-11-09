0dfe02e4b15c70cac129d4bd3371dc75cc8a6ef1
Luis-Betancourt93 <betancourtluis619@gmail.com>
2023-11-08 19:31:14 -0800

    The Hashtag Generator

---
+++ JS-Challenges/46-Hashtag-Generator.js
@@ -0,0 +1,21 @@
+/*
+The marketing team is spending way too much time typing in hashtags. Let's help them with our own Hashtag Generator! Here's the deal:
+
+It must start with a hashtag #.
+Ignore spaces in the input.
+All words must have their first letter capitalized.
+If the final result is longer than 140 chars it must return false.
+If the input or the result is an empty string it must return false.
+*/
+
+
+const generateHashtag = str => {
+  const hashtag = str
+    .split(' ')
+    .reduce(
+      (tag, word) => tag + word.charAt(0).toUpperCase() + word.slice(1),
+      '#'
+    );
+
+    return hashtag.length === 1 || hashtag.length > 140 ? false : hashtag;
+  };
