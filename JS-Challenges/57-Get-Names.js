82bbf733fb7985b16d022e17348519956beb3ee3
Luis-Betancourt93 <betancourtluis619@gmail.com>
2023-11-20 22:42:28 -0800

    Get names

---
+++ JS-Challenges/57-Get-Names.js
@@ -0,0 +1,6 @@
+/*
+Write a function that given an array of users, returns an array of their names. Can you achieve this in one line of code?
+*/
+
+
+const getNames = users => users.map(user => user.name);
