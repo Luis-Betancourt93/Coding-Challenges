6b0942886421fbd19f96373ae7000b80b80e7322
Luis-Betancourt93 <betancourtluis619@gmail.com>
2023-11-28 15:12:32 -0800

    Make number opposite

---
+++ JS-Challenges/Opposite-Number.js
@@ -0,0 +1,26 @@
+//Very simple, given an integer or a floating-point number, find its opposite.
+
+/*
+1: -1
+14: -14
+-34: 34
+
+*/
+
+
+function opposite(number) {
+  /*
+  if (number < 0) {
+    return number * -1
+  } else {
+    return number * -1 
+  }
+*/
+
+return number * -1 
+
+}
+
+console.log(opposite(5)) // -5
+console.log(opposite(-10)) // 10 
+console.log(opposite(22)) // -22 
