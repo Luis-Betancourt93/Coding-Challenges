38f68a026eeb07bf1276221dde23a32eb551cd4d
Luis-Betancourt93 <betancourtluis619@gmail.com>
2024-01-04 14:16:32 -0800

    Made sure the button1 will never be reassign

--- Free-Code/Role-Playing-Game/js/index.js
+++ Free-Code/Role-Playing-Game/js/index.js
@@ -9,4 +9,4 @@ let fighting;
 let monsterHealth;
 let inventory = ["stick", "dagger", "sword"];
 
-let button1 = document.querySelector('#button1');
+const button1 = document.querySelector('#button1');
