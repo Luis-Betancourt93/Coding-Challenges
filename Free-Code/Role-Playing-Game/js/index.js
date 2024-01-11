b0d482a38c518ba131b1727fb7021a413a7c799b
Luis-Betancourt93 <betancourtluis619@gmail.com>
2024-01-11 07:42:00 -0800

    The goStore button will now let you buy gold

--- Free-Code/Role-Playing-Game/js/index.js
+++ Free-Code/Role-Playing-Game/js/index.js
@@ -39,3 +39,7 @@ function fightDragon() {
 button1.onclick = goStore;
 button2.onclick = goCave;
 button3.onclick = fightDragon;
+
+function goStore() {
+  button1.innerText = 'Buy 10 health (10 gold)'
+}
