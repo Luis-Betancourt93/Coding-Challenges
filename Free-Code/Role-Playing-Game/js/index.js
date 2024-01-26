69f4ad571b93b591453aeba615e859da6ea1d634
Luis-Betancourt93 <betancourtluis619@gmail.com>
2024-01-25 16:49:49 -0800

    Made another property call button functions

--- Free-Code/Role-Playing-Game/js/index.js
+++ Free-Code/Role-Playing-Game/js/index.js
@@ -78,5 +78,6 @@ function update(location) {
 
 const locations = [{
   name: 'town square',
-  "button text": ["Go to store", "Go to cave", "Fight dragon"]
+  "button text": ["Go to store", "Go to cave", "Fight dragon"],
+  "button functions" : [goStore, goCave, fightDragon]
 }];
