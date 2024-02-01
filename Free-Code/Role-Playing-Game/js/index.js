171d4c152aa937e52b2bb7765c94af18f143b154
Luis-Betancourt93 <betancourtluis619@gmail.com>
2024-01-31 21:18:04 -0800

    Updated the updated funtions

--- Free-Code/Role-Playing-Game/js/index.js
+++ Free-Code/Role-Playing-Game/js/index.js
@@ -75,7 +75,13 @@ function buyHealth() {
 
 
 function update(location) {
-
+  button1.innerText = location['button text'];
+  button2.innerText = "Go to cave";
+  button3.innerText = "Fight dragon";
+  button1.onclick = goStore;
+  button2.onclick = goCave;
+  button3.onclick = fightDragon;
+  text.innerText = "You are in the town square. You see a sign that says \"Store\".";
 }
 
 const locations = [{
