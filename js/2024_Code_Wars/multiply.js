92434ae05f13114564facfa05f35387aa672596d
Luis-Betancourt93 <betancourtluis619@gmail.com>
2024-02-08 21:57:19 -0800

    2024 Code Wars JavaScript

---
+++ js/2024_Code_Wars/multiply.js
@@ -0,0 +1,16 @@
+/*
+  This code does not execute properly. Try to figure out why.
+  
+  function multiply(a, b){
+  a * b
+}
+
+*/
+
+
+// Must have the 'return' keyword
+function multiply(a, b){
+  return a * b
+}
+
+console.log(multiply(1,1))
