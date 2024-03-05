c63ef03b39507b9aeaac785690d7bbff6ce2749b
Luis-Betancourt93 <betancourtluis619@gmail.com>
2024-03-05 13:32:48 -0800

    Taking it back to the basics

---
+++ js/2024_Code_Wars/Function1HelloWorld.js
@@ -0,0 +1,14 @@
+/*
+Make a simple function called greet that returns the most-famous "hello world!".
+
+*/
+
+
+// function greet(string) {
+//   console.log(string)
+// }
+
+
+const greet = string => console.log(string)
+
+greet('Hello World')
