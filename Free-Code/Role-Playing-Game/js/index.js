e65a622ef45f0a1f03626aafefbf28e59d32f70d
Luis-Betancourt93 <betancourtluis619@gmail.com>
2024-03-14 14:21:47 -0700

    Added 4 new weapons

--- Free-Code/Role-Playing-Game/js/index.js
+++ Free-Code/Role-Playing-Game/js/index.js
@@ -103,8 +103,24 @@ function update(location) {
   text.innerText = text.innerText = location.text;;
 }
 
-const weapons = []
-
+const weapons = [
+  {
+    name: "stick",
+    power: 5
+  },
+  {
+    name: "dagger",
+    power: 30
+  },
+  {
+    name: "claw hammer",
+    power: 50
+  },
+   {
+     name: "sword",
+     power: 100
+  }
+];
 
 const locations = [{
   name: 'town square',
