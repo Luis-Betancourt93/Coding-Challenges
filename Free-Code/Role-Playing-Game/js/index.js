06cd91e0b75e3f1b8cd7092119f36c128ee90137
Luis-Betancourt93 <betancourtluis619@gmail.com>
2024-03-23 11:34:30 -0700

    Change the inner text when buying a new weapon

--- Free-Code/Role-Playing-Game/js/index.js
+++ Free-Code/Role-Playing-Game/js/index.js
@@ -85,7 +85,7 @@ function buyHealth() {
     currentWeapon ++;
     goldText.innerText = gold;
     let newWeapon = weapons[currentWeapon].name;
-    text.innerText = "You now have a new weapon.";
+    text.innerText = "You now have a " + newWeapon + '.';
   }
  }
 
