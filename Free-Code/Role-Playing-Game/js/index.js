404f2e8630e9af8867a55f3fef393ec27b15a45f
Luis-Betancourt93 <betancourtluis619@gmail.com>
2024-03-27 11:51:32 -0700

    Added a else statment incase the player didnt have enough money to buy a weapon

--- Free-Code/Role-Playing-Game/js/index.js
+++ Free-Code/Role-Playing-Game/js/index.js
@@ -88,6 +88,8 @@ function buyHealth() {
     text.innerText = "You now have a " + newWeapon + '.';
     inventory.push(newWeapon);
     text.innerText += ' In your inventory you have: ' + inventory; 
+  } else {
+    text.innerText = 'You do not have enough gold to buy a weapon.'
   }
  }
 
