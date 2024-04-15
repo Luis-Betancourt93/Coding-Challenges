094a89d7f741309dea949c51e68d5a9ed689034f
Luis-Betancourt93 <betancourtluis619@gmail.com>
2024-04-15 12:40:06 -0700

    Made it possible to sell your weapon for gold

--- Free-Code/Role-Playing-Game/js/index.js
+++ Free-Code/Role-Playing-Game/js/index.js
@@ -94,6 +94,8 @@ function buyHealth() {
     }
   } else {
     text.innerText = 'You already have the most powerful weapon!'
+    button2.innerText = 'Sell weapon for 15 gold'
+    button2.onclick = sellWeapon;
   } 
  }
 
