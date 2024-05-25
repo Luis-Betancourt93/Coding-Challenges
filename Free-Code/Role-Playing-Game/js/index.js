a8689b44b00ca4b3e79fd51be16feb210840bd71
Luis-Betancourt93 <betancourtluis619@gmail.com>
2024-05-25 15:54:41 -0700

    Made a dodge function and the text will change when you dodge an attack from the monster

--- Free-Code/Role-Playing-Game/js/index.js
+++ Free-Code/Role-Playing-Game/js/index.js
@@ -73,12 +73,16 @@ function attack() {
   }
 }
 
+function dodge() {
+  text.innerText = "You dodge the attack from the " + monsters[fighting].name + "."
+}
+
 function defeatMonster() {
 
 }
 
 function lose() {
-  
+
 }
   
 
