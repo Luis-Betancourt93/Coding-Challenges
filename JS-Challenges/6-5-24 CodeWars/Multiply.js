d7cf5ac419ed9f708cdb1fba226c31f88e62e153
Luis-Betancourt93 <betancourtluis619@gmail.com>
2024-06-06 09:21:54 -0700

    Moved some files around and started codewars

---
+++ JS-Challenges/6-5-24 CodeWars/Multiply.js
@@ -0,0 +1,24 @@
+/*
+This code does not execute properly. Try to figure out why.
+*/
+
+/*
+function multiply(a, b){
+ console.log(a * b)
+}
+*/
+
+/*
+function multiply(a,b) {
+ return a * b
+}
+*/
+
+// const multiply = (a,b) => console.log(a * b)
+
+
+
+
+multiply(2, 5); // 10
+multiply(5, 10); // 50 
+multiply(0, 100); // 0 
