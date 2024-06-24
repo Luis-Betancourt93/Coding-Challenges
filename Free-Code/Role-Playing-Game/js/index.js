d3a372cad0cba6d13405e632c304b4486bff24a0
Luis-Betancourt93 <betancourtluis619@gmail.com>
2024-06-23 19:40:23 -0700

    Added a else statment to the Monster hit incase you miss him

--- Free-Code/Role-Playing-Game/js/index.js
+++ Free-Code/Role-Playing-Game/js/index.js
@@ -64,6 +64,8 @@ function attack() {
   health -= getMonsterAttackValue(monsters[fighting].level);
   if(isMonsterHit()) {
     monsterHealth -= weapons[currentWeapon].power + Math.floor(Math.random() * xp) + 1;
+  } else {
+    text.innerText += ' You miss.'
   }
 
   
