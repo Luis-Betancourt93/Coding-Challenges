154fcd7e4edee3b5b0618b9f2b130502cebce211
Luis-Betancourt93 <betancourtluis619@gmail.com>
2024-08-23 18:24:42 -0700

    Made where if the user pauses the song it will return to the same place it stoped

--- Free-Code/Music-Player/script.js
+++ Free-Code/Music-Player/script.js
@@ -133,4 +133,6 @@ const playSong = id => {
 
 if(userData?.currentSong === null || userData?.currentSong.id !== song.id) {
   audio.currentTime = 0;
+} else {
+  audio.currentTime = userData?.songCurrentTime;
 }
