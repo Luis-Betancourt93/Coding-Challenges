038183c953ccc4cbca0ca377b9effbf1525a6138
Luis-Betancourt93 <betancourtluis619@gmail.com>
2024-09-23 13:34:18 -0700

    got the element by id for the song artist

--- Free-Code/Music-Player/script.js
+++ Free-Code/Music-Player/script.js
@@ -210,4 +210,5 @@ highlightCurrentSong();
 
 const setPlayerDisplay= () => {
   const playingSong = document.getElementById('player-song-title');
+  const songArtist = document.getElementById("player-song-artist");
 };
