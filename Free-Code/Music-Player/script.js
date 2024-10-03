ad1f9d36d75703c962568d6ea0f892e97426a279
Luis-Betancourt93 <betancourtluis619@gmail.com>
2024-10-02 20:41:03 -0700

    Called the function inside the playSong function

--- Free-Code/Music-Player/script.js
+++ Free-Code/Music-Player/script.js
@@ -223,4 +223,7 @@ setPlayerDisplay();
 const setPlayButtonAccessibleText = () => {
   const song = userData?.currentSong || userData?.songs[0];
   playButton.setAttribute("aria-label", song?.title ? `Play ${song.title}`: 'Play')
+
 }
+
+setPlayButtonAccessibleText();
