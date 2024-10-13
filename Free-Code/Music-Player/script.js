2c47e4eb6a665f30c7b3a8e97a6487f303d0cb31
Luis-Betancourt93 <betancourtluis619@gmail.com>
2024-10-13 11:22:09 -0700

    If there is a match then set the user data to null and the time to 0

--- Free-Code/Music-Player/script.js
+++ Free-Code/Music-Player/script.js
@@ -249,7 +249,10 @@ const deleteSong = id => {
   setPlayButtonAccessibleText();
 
   if (userData?.currentSong?.id === id) {
-    
+    userData.currentSong = null;
+    userData.songCurrentTime = 0;
+    pauseSong();
+    setPlayerDisplay();
   } else {
     
   }
