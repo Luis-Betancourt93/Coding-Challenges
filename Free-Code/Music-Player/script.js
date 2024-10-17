0b6874bc430fc43f29ce5c421f95817036ae4203
Luis-Betancourt93 <betancourtluis619@gmail.com>
2024-10-17 11:36:06 -0700

    After making a reset button I added a text to that node

--- Free-Code/Music-Player/script.js
+++ Free-Code/Music-Player/script.js
@@ -260,4 +260,5 @@ const deleteSong = id => {
 
 if(userData?.songs.length === 0 ) {
  const resetButton = document.createElement("button");
+ const resetText = document.createTextNode("Reset Playlist");
 }
