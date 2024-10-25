9d582ed09b5da9b11430c523a369372c5ad82bfc
Luis-Betancourt93 <betancourtluis619@gmail.com>
2024-10-25 12:04:59 -0700

    Added a if statement to check if next song exist and if it did to go ahead and play it

--- Free-Code/Music-Player/script.js
+++ Free-Code/Music-Player/script.js
@@ -280,4 +280,8 @@ resetButton.addEventListener("click", () =>  {
 audio.addEventListener('ended', () => {
   const currentSongIndex = getCurrentSongIndex();
   const nextSongExists = currentSongIndex <= userData.songs.length - 1;
+
+  if(nextSongExists) {
+    playNextSong();
+  }
 })
