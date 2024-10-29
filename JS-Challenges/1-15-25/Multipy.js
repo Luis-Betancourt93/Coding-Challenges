efb60b53f2d30b0091c8794a2e7b5fa417e0970a
Luis-Betancourt93 <betancourtluis619@gmail.com>
2024-10-29 12:22:38 -0700

    Turn the solution of multiply into a arrow function and added a test case

--- JS-Challenges/1-15-25/Multipy.js
+++ JS-Challenges/1-15-25/Multipy.js
@@ -1,7 +1,10 @@
 //This code does not execute properly. Try to figure out why.
 
-
+/*
 function multiply(a, b){
   return a * b
 }
+*/
+const multiply = (a,b) => console.log(a * b);
 
+multiply(10,10);
