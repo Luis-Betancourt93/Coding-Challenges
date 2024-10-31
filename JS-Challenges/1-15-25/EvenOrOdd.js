4bea2cfad252501fe2b59d0ead2e2704d0a1c902
Luis-Betancourt93 <betancourtluis619@gmail.com>
2024-10-31 12:36:48 -0700

    Turn the even or odd solution into a arrow function and added a case statement

--- JS-Challenges/1-15-25/EvenOrOdd.js
+++ JS-Challenges/1-15-25/EvenOrOdd.js
@@ -1,6 +1,6 @@
 //Create a function that takes an integer as an argument and returns "Even" for even numbers or "Odd" for odd numbers.
 
-
+/*
 function evenOrOdd(number) {
   if (number % 2 == 0) {
     return 'Even'
@@ -8,3 +8,7 @@ function evenOrOdd(number) {
     return 'Odd'
   }
 }
+ */
+const evenOrOdd = number => number % 2 === 0? console.log('True') : console.log('False');
+
+evenOrOdd(1)
