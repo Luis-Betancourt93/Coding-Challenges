56885c29f5d90e7be160252a0ebd67e9cbe0c0e6
Luis-Betancourt93 <betancourtluis619@gmail.com>
2024-11-04 12:59:23 -0800

    Made a variable for cart, products, and dessert

---
+++ Free-Code/Shopping-Cart/script.js
@@ -0,0 +1,4 @@
+
+const cartContainer = document.getElementById("cart-container");
+const productsContainer = document.getElementById("products-container");
+const dessertContainer = document.getElementById("dessert-container");
