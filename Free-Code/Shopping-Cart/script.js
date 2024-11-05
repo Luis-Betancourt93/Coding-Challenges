5117f0a164b2eeecccf31fe7fff9205c578bb93b
Luis-Betancourt93 <betancourtluis619@gmail.com>
2024-11-05 13:03:10 -0800

    Made 2 buttons to clear the cart

--- Free-Code/Shopping-Cart/script.js
+++ Free-Code/Shopping-Cart/script.js
@@ -2,3 +2,7 @@
 const cartContainer = document.getElementById("cart-container");
 const productsContainer = document.getElementById("products-container");
 const dessertContainer = document.getElementById("dessert-container");
+
+const dessertCards = document.getElementById('dessert-car-container');
+const cartBtn = document.getElementById('cart-btn');
+const clearCartBtn = document.getElementById('clear-cart-btn');
