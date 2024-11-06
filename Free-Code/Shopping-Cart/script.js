70e7f8b1c909e9da4b213f22245be97560b1309d
Luis-Betancourt93 <betancourtluis619@gmail.com>
2024-11-06 13:06:48 -0800

    Getting all the totals

--- Free-Code/Shopping-Cart/script.js
+++ Free-Code/Shopping-Cart/script.js
@@ -1,8 +1,11 @@
 
 const cartContainer = document.getElementById("cart-container");
 const productsContainer = document.getElementById("products-container");
-const dessertContainer = document.getElementById("dessert-container");
-
 const dessertCards = document.getElementById('dessert-car-container');
 const cartBtn = document.getElementById('cart-btn');
 const clearCartBtn = document.getElementById('clear-cart-btn');
+
+const totalNumberOfItems = document.getElementById("total-items");
+const cartSubTotal = document.getElementById("subtotal");
+const cartTaxes = document.getElementById('taxes');
+const cartTotal = document.getElementById('total');
