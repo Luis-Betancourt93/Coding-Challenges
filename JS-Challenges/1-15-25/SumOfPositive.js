4eeb91140ae85f3b9cdb03235ec96770f8e2e4f1
Luis-Betancourt93 <betancourtluis619@gmail.com>
2025-03-15 16:00:38 -0700

    Sum of Positive arrow function

--- JS-Challenges/1-15-25/SumOfPositive.js
+++ JS-Challenges/1-15-25/SumOfPositive.js
@@ -16,10 +16,13 @@ Note
 If there is nothing to sum, the sum is default to 0.
 */
 
-
+/*
 function positiveSum(arr) {
   return arr.filter(num => num > 0).reduce((accumulator, currentValue) => accumulator + currentValue, 0)
 }
+*/
+//Arrow Function
+  const positiveSum = arr => arr.filter(num => num > 0).reduce((acc,current) => acc + current, 0 )
 
 console.log(positiveSum([1,2,3,4,5])) //15
 console.log(positiveSum([1,-2,3,4,5])) //13
