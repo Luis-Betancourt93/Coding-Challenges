67ac849e8c5d10d07887a54a01dac9d2b39b658a
Luis-Betancourt93 <betancourtluis619@gmail.com>
2025-03-16 19:42:17 -0700

    String repeat

---
+++ JS-Challenges/1-15-25/StringRepeat.js
@@ -0,0 +1,22 @@
+/*
+Write a function that accepts a non-negative integer n and a string s as parameters, and returns a string of s repeated exactly n times.
+
+Examples (input -> output)
+6, "I"     -> "IIIIII"
+5, "Hello" -> "HelloHelloHelloHelloHello"
+
+*/
+
+function repeatStr (n, s) {
+  if (n > 0) {
+    return s.repeat(n)
+  } else {
+    return ''
+  }
+}
+
+console.log(repeatStr(3, "*"))
+console.log(repeatStr(5, "#"))
+console.log(repeatStr(2, "ha "))
+console.log(repeatStr(0, ""))
+console.log(repeatStr(0, "I"))
