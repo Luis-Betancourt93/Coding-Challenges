b0c2d0cb11b45d254bc752cbbf3db3b258798356
Luis-Betancourt93 <betancourtluis619@gmail.com>
2025-03-27 20:40:35 -0700

    Sum of two nums arrow function

--- JS-Challenges/edabit/SumOfTwoNums.js
+++ JS-Challenges/edabit/SumOfTwoNums.js
@@ -5,9 +5,13 @@ Create a function that takes two numbers as arguments and returns their sum.
 
 */
 
+/*
 function addition(a, b) {
   if (typeof a !== "number" || typeof b !== "number") {
       return "Sorry but you didn't pass two numbers.";
   }
   return a + b;
 }
+  */
+
+const addition = (a, b) => a + b;
