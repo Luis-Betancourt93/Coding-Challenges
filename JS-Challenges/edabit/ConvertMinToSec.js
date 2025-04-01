1e26781faea0f0cc2d70b8c05f530adae308b155
Luis-Betancourt93 <betancourtluis619@gmail.com>
2025-03-31 20:43:07 -0700

    Convert hr to mins arrow funtion

--- JS-Challenges/edabit/ConvertMinToSec.js
+++ JS-Challenges/edabit/ConvertMinToSec.js
@@ -2,6 +2,11 @@
 Write a function that takes an integer minutes and converts it to seconds.
 */
 
+/*
 function convert(minutes) {
 	return  minutes * 60;
 }
+  */
+
+
+const convert = minutes => minutes * 60;
