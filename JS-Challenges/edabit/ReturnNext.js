b228b1fb0bda9b7f4b5db19156ccae0e3352ffb6
Luis-Betancourt93 <betancourtluis619@gmail.com>
2025-04-02 20:45:43 -0700

    Return next num arrow funtion

--- JS-Challenges/edabit/ReturnNext.js
+++ JS-Challenges/edabit/ReturnNext.js
@@ -2,6 +2,10 @@
 Create a function that takes a number as an argument, increments the number by +1 and returns the result.
 */
 
+/*
 function addition(num) {
 	return ++num
 }
+*/
+
+const addition = (num) => num + 1
