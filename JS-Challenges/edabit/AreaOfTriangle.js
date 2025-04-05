c0a620cfd847ed49462aea8b95585d485b7e556c
Luis-Betancourt93 <betancourtluis619@gmail.com>
2025-04-04 20:48:20 -0700

    Get area from triangle arrow function

--- JS-Challenges/edabit/AreaOfTriangle.js
+++ JS-Challenges/edabit/AreaOfTriangle.js
@@ -2,6 +2,9 @@
 Write a function that takes the base and height of a triangle and return its area.
 */
 
+/*
 function triArea(base, height) {
 	return base * height /2
 }
+*/
+const triArea = (base, height) => (base * height) / 2;
