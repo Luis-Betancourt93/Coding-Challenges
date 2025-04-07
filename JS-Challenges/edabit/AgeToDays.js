eda6a34b9ce23e064bbe23251e7acb9ae520f7d6
Luis-Betancourt93 <betancourtluis619@gmail.com>
2025-04-06 20:50:39 -0700

    Age to Days arrow function

--- JS-Challenges/edabit/AgeToDays.js
+++ JS-Challenges/edabit/AgeToDays.js
@@ -1,8 +1,11 @@
 /*
 Create a function that takes the age in years and returns the age in days.
 */
-
+/*
 function calcAge(age) {
 	let ageInDays = (age*365)>=0 && typeof(age*365) == 'number' ? (age*365) : []
 	return ageInDays
 }
+*/
+
+const calcAge = age => age * 365;
