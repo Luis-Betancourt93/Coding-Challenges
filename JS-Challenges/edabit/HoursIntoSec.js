be42c2d21a2be270b208571f6a1629c36e467215
Luis-Betancourt93 <betancourtluis619@gmail.com>
2025-04-14 17:06:03 -0700

    Hours into sec arrow function

--- JS-Challenges/edabit/HoursIntoSec.js
+++ JS-Challenges/edabit/HoursIntoSec.js
@@ -6,3 +6,7 @@ Write a function that converts hours into seconds.
 function howManySeconds(hours) {
 	return hours * 3600;
 }
+
+/*
+const howManySeconds = hours => Number.isInteger(hours) ? hours*3600 : "error";
+*/
