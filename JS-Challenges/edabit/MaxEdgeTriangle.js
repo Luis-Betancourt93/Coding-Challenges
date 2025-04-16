a0a28476151fbdb7f05df309d6b868c1d3637071
Luis-Betancourt93 <betancourtluis619@gmail.com>
2025-04-15 17:07:30 -0700

    Maximum edge of a triangle

---
+++ JS-Challenges/edabit/MaxEdgeTriangle.js
@@ -0,0 +1,11 @@
+/*
+Create a function that finds the maximum range of a triangle's third edge, where the side lengths are all integers.
+*/
+
+function nextEdge(side1, side2) {
+	if(side1 > 0 && side2 > 0) {
+		return (side1 + side2) - 1;
+	}else {
+		return;
+	}
+}
