8361271960482d62302cc209b23ebe9cd8491615
Luis-Betancourt93 <betancourtluis619@gmail.com>
2025-04-20 17:12:03 -0700

    Find the perimeter of a rectangle arrow function

--- JS-Challenges/edabit/FindThePerimeter.js
+++ JS-Challenges/edabit/FindThePerimeter.js
@@ -5,3 +5,7 @@ Create a function that takes length and width and finds the perimeter of a recta
 function findPerimeter(length,width){
 	return (length + width) * 2;
 }
+
+/*
+const findPerimeter = (h, w) => (w+h)*2
+*/
