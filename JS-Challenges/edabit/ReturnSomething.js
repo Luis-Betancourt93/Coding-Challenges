63b34691c556bb2c2a8d9fa3c350031078ad63a8
Luis-Betancourt93 <betancourtluis619@gmail.com>
2025-04-22 17:13:44 -0700

    Return something to me arrow function

--- JS-Challenges/edabit/ReturnSomething.js
+++ JS-Challenges/edabit/ReturnSomething.js
@@ -5,3 +5,7 @@ Write a function that returns the string "something" joined with a space " " and
 function giveMeSomething(a) {
   return `something ${a}`;
  }
+
+ /*
+const giveMeSomething = a => `something ${a}`;
+ */
