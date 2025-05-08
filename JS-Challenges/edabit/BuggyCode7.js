b28bdb17054a4bf066f5ea3581dd333f873e3d70
Luis-Betancourt93 <betancourtluis619@gmail.com>
2025-05-07 17:31:17 -0700

    buggy code pt7

---
+++ JS-Challenges/edabit/BuggyCode7.js
@@ -0,0 +1,17 @@
+/*
+Mubashir wants to swap two given numbers!
+
+It is not returning the right values. Can you help him fix it?
+*/
+function swap(a, b){
+	t = a // Store value of `a` in a temporary variable `t`
+	a = b // Store value of `b` in `a`
+	b = t // Store value of `t` in `b`
+	return [a, b]
+}
+
+
+
+/*
+
+*/
