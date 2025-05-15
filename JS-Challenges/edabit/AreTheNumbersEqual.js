1b6ad85367ab5d2129b6a2aa25892d734a4cd868
Luis-Betancourt93 <betancourtluis619@gmail.com>
2025-05-14 17:37:28 -0700

    Are the nums equal? arrow function

--- JS-Challenges/edabit/AreTheNumbersEqual.js
+++ JS-Challenges/edabit/AreTheNumbersEqual.js
@@ -6,3 +6,7 @@ Create a function that returns true when num1 is equal to num2; otherwise return
 function isSameNum(num1, num2) {
 	return num1 === num2
 }
+
+/*
+const isSameNum = (num1, num2) => num1 === num2;
+*/
