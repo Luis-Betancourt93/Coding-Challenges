4c79da8f21892a0240b70b4c8b66cba2a36b1d44
Luis-Betancourt93 <betancourtluis619@gmail.com>
2025-05-18 17:42:00 -0700

    Conver Hours to min to sec arrow function

--- JS-Challenges/edabit/HoursToMinToSec.js
+++ JS-Challenges/edabit/HoursToMinToSec.js
@@ -5,3 +5,6 @@ Write a function that takes two integers (hours, minutes), converts them to seco
 function convert(hours, minutes) {
 	return ((hours*60) + minutes) * 60
 }
+/*
+const convert = (hours, minutes) => hours * 3600 + minutes * 60
+*/
