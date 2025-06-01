3aed07969342d7fbc3ebbf23672bbf2cad4ed887
Luis-Betancourt93 <betancourtluis619@gmail.com>
2025-05-31 17:56:24 -0700

    buggy code 4

---
+++ JS-Challenges/edabit/buggyCode4.js
@@ -0,0 +1,9 @@
+/*
+Emmy has written a function that returns a greeting to users. However, she's in love with Mubashir, and would like to greet him slightly differently. She added a special case in her function, but she made a mistake.
+
+Can you help her?
+*/
+
+function greeting(name){
+  return name == "Mubashir"?"Hello, my Love!":"Hello, " + name + "!";
+  }
