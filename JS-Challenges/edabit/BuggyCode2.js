d41a03e2a1b027cca31c809cdf331a6adfa47d4b
Luis-Betancourt93 <betancourtluis619@gmail.com>
2025-06-06 18:07:30 -0700

    buggy code 2

---
+++ JS-Challenges/edabit/BuggyCode2.js
@@ -0,0 +1,12 @@
+/*
+Fix the code in the code tab to pass this challenge (only syntax errors). Look at the examples below to get an idea of what the function should do.
+*/
+
+function maxNum(n1,n2) {
+	if (n2>n1){
+	  return n2;
+	}
+  else{
+	return n1;
+  }
+}
