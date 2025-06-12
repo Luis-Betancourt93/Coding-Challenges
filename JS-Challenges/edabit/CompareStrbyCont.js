86d250016fe438315d9a21e6a99c24d95f88fa46
Luis-Betancourt93 <betancourtluis619@gmail.com>
2025-06-11 18:11:43 -0700

    Compare string by count arrow function

--- JS-Challenges/edabit/CompareStrbyCont.js
+++ JS-Challenges/edabit/CompareStrbyCont.js
@@ -7,3 +7,6 @@ Create a function that takes two strings as arguments and return either true or 
 function comp(str1, str2) {
 	return str1.length === str2.length;
 }
+/*
+const comp = (str1, str2) => str1.length === str2.length;
+*/
