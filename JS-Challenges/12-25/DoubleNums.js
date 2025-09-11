b2e1dcf7d005fbc4eae3fd49e2b2fe790ef2f0c7
Luis-Betancourt93 <betancourtluis619@gmail.com>
2025-09-11 10:56:14 -0700

    DoubleNums

---
+++ JS-Challenges/12-25/DoubleNums.js
@@ -0,0 +1,7 @@
+//Instruction: Write a map function that returns an array where every number is divided by 2.
+//Target: [5, 10, 15, 20, 25]
+
+
+const numbers = [10, 20, 30, 40, 50];
+const numDivided = numbers.map(element => element / 2);
+console.log(numDivided);
