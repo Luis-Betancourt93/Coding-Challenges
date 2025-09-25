49a208f2ea6588ad3be667bea59bad60d3a4883b
Luis-Betancourt93 <betancourtluis619@gmail.com>
2025-09-25 16:09:38 -0700

    FirstItem

---
+++ JS-Challenges/12-25/FirstItem.js
@@ -0,0 +1,18 @@
+/*
+Instruction: Write a map function that returns an array of objects containing the title and the primary genre (the first item in the genres array).
+
+Target: [{ title: "Frozen", primary_genre: "Animation" }, { title: "John Wick", primary_genre: "Action" }, ...]
+*/
+
+const movies = [
+  { title: "Frozen", genres: ["Animation", "Family", "Musical"] },
+  { title: "John Wick", genres: ["Action", "Thriller"] },
+  { title: "Notebook", genres: ["Romance", "Drama"] }
+];
+
+const firstItem = movies.map(element => ({
+  title: element.title, primary_genre: element.genres[0]
+
+}));
+
+console.log(firstItem);
